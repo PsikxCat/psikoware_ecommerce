85a4f77e1d26a0c0a0ce82e5f3edd19a3765a05c
PsikxCat <arevalogrichard@gmail.com>
2024-01-30 18:41:58 -0500

    Agregar orden de pago a base de datos

--- src/app/api/create-payment/route.ts
+++ src/app/api/create-payment/route.ts
@@ -2,53 +2,56 @@ import { type NextRequest } from 'next/server'
 import { MercadoPagoConfig, Payment } from 'mercadopago'
 import db from '@/libs/prismadb'
 
-import { getCurrentUser } from '@/libs/actions/getCurrentUser'
-
 const mercadopago = new MercadoPagoConfig({ accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN! })
 
 export async function POST(req: NextRequest) {
-  const body = await req.json()
-  console.log('body --->', body)
-
-  const paymentId = body.data.id
-  const payment = await new Payment(mercadopago).get({ id: paymentId })
-  console.log('payment --->', payment)
-
-  const currentUserId = payment?.external_reference
-  console.log('currentUserId --->', currentUserId)
-
-  // const products = payment.additional_info!.items.map((item: any) => {
-
-  const orderData = {
-    user: { connect: { id: currentUserId } },
-    paymentId,
-    amount: Number(payment.transaction_amount),
-    currency: payment.currency_id as string,
-    status: payment.status as string,
-    deliveryStatus: 'pending',
-    products: payment.additional_info!.items
-  }
-  console.log('orderData --->', orderData)
-
-  // if (payment) {
-  //   // actualizar data de order en db
-  // } else {
-  //   // crear order en mongo mediante prisma
-  //   const order = await db.order.create({ data: orderData })
-  //   console.log('order --->', order)
-  // }
-
-  return Response.json({ message: 'Payment created', ok: true, status: 201 })
-}
-
-// request para tests
-export async function GET() {
   try {
-    const currentUser = await getCurrentUser()
-    console.log('currentUser --->', currentUser)
+    const body = await req.json()
 
-    return Response.json({ message: currentUser, ok: true })
+    const paymentId = body.data.id
+    const payment = await new Payment(mercadopago).get({ id: paymentId })
+
+    const currentUserId = payment?.external_reference
+
+    const products = payment.additional_info!.items?.map((item) => {
+      return {
+        id: item.id,
+        unitPrice: Number(item.unit_price),
+        name: item.title,
+        category: item.category_id as string,
+        quantity: Number(item.quantity),
+        selectedImg: item.picture_url as string
+      }
+    })
+
+    const orderData = {
+      user: { connect: { id: currentUserId } },
+      paymentId,
+      amount: Number(payment.transaction_amount),
+      currency: payment.currency_id as string,
+      status: payment.status as string,
+      deliveryStatus: 'pending',
+      products
+    }
+
+    const order = await db.order.create({ data: orderData })
+    console.log('order --->', order)
+
+    return Response.json({ message: 'Payment created', ok: true, status: 201 })
   } catch (error) {
-    return Response.json({ message: error, ok: false })
+    console.error('Error al procesar el pago', error)
+    return Response.json({ message: error, ok: false, status: 500 })
   }
 }
+
+// request para test
+// export async function GET() {
+//   try {
+//     const currentUser = await getCurrentUser()
+//     console.log('currentUser --->', currentUser)
+
+//     return Response.json({ message: currentUser, ok: true })
+//   } catch (error) {
+//     return Response.json({ message: error, ok: false })
+//   }
+// }
